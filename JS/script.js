5adb675c6aabdfaadc43f2a3ce1d50c5c77eb90c
RehamHammad <hammadreham8@gmail.com>
2025-06-15 22:17:58 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,60 @@
+// const keyApi='6ea6bc9b8d904ba79f8114752222310';
+// const baseURL='http://api.weatherapi.com/v1';
+
+//? API URL 
+// https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=london&days=3
+
+//Variables
+let searchInput=document.querySelector('#searchInput');
+let weatherData;
+
+
+//Functions
+searchInput.addEventListener('input',function(){
+    if(searchInput.value.length > 3){
+        startApp(searchInput.value);
+    }
+})
+async function startApp(city){
+ weatherData=await getData(city)
+ console.log(weatherData);
+ todayWeather();
+ tomorrowWeather();
+ afterTomorrowWeather();
+}
+async function getData(city){
+    let response=await fetch(`https://api.weatherapi.com/v1/forecast.json?key=ab665c02dc384b38902150930242803&q=${city}&days=3`);
+    let data=await response.json();
+    return data;
+    
+}
+
+function todayWeather(){
+ document.querySelector('.today .city').innerHTML=weatherData.location.name;
+ document.querySelector('.today .degree').innerHTML=weatherData.current.temp_c + 'C';
+ document.querySelector('.today #todayImg').setAttribute('src',weatherData.current.condition.icon);
+ document.querySelector('.today .weatherCondition').innerHTML=weatherData.current.condition.text;
+ document.querySelector('.today .humidity #humidity').innerHTML=weatherData.current.humidity;
+ document.querySelector('.today .winds #winds').innerHTML=weatherData.current.wind_mph;
+}
+function tomorrowWeather(){
+    document.querySelector('.tomorrow .forecast-body .degree #tomImg').setAttribute('src','https:' + weatherData.forecast.forecastday[1].day.condition.icon)
+    document.querySelector('.tomorrow .maxTemperature').innerHTML=weatherData.forecast.forecastday[1].day.maxtemp_c;
+    document.querySelector('.tomorrow .minTemperature').innerHTML=weatherData.forecast.forecastday[1].day.mintemp_c;
+    document.querySelector('.tomorrow .weatherCondition').innerHTML=weatherData.forecast.forecastday[1].day.condition.text
+}
+
+function afterTomorrowWeather(){
+  document.querySelector('.after-tomorrow #afterimage').setAttribute('src','https:' + weatherData.forecast.forecastday[2].day.condition.icon)
+   document.querySelector('.after-tomorrow .maxTemperature').innerHTML=weatherData.forecast.forecastday[2].day.maxtemp_c;
+    document.querySelector('.after-tomorrow .minTemperature').innerHTML=weatherData.forecast.forecastday[2].day.mintemp_c;
+    document.querySelector('.after-tomorrow .weatherCondition').innerHTML=weatherData.forecast.forecastday[2].day.condition.text
+}
+
+//Live Location
+navigator.geolocation.getCurrentPosition(position =>{
+    liveLocation=position.coords.latitude + ',' + position.coords.longitude;
+    console.log(liveLocation);
+    startApp(liveLocation)
+    
+})
